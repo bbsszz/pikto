b6a285689b24f87f627c09be1002ec85092d3445
IIUJ-CezaryWalczak <cezary.walczak@uj.edu.pl>
2012-12-11 21:59:02 +0100

    Task 2 of the second iteration implemented (Pictogram Classifier).

---
+++ PiktoSolution/PiktoProject/RecognitionPath/Classifier/ART1PictogramClassifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdaptiveResonanceTheory1;
+
+namespace Pikto.RecognitionPath.Classifier
+{
+	class ART1PictogramClassifier : IPictogramClassifier
+	{
+		private ART1 art1Network;
+
+		public ART1PictogramClassifier(ART1 art1Network)
+		{
+			this.art1Network = art1Network;
+		}
+
+		public int Classify(byte[,] image, bool forceLearning = false)
+		{
+			return art1Network.Present(image.Cast<float>(), forceLearning);
+		}
+	}
+}
---
+++ PiktoSolution/PiktoProject/RecognitionPath/Classifier/IPictogramClassifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pikto.RecognitionPath.Classifier
+{
+	interface IPictogramClassifier
+	{
+		int Classify(byte[,] image, bool forceLearning = false);
+	}
+}
