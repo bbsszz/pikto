fd56d8ea38a9676e4f047858415ff342fd869970
Pawel Penar <pawelpq@gmail.com>
2013-06-12 11:55:15 +0200

    ISS 27 Pierwsze podejscie

--- PiktoSolution/PiktoProject/View/LearningPathWindow.xaml.cs
+++ PiktoSolution/PiktoProject/View/LearningPathWindow.xaml.cs
@@ -23,59 +23,38 @@ namespace Pikto.View
 	/// </summary>
 	public partial class LearningPathWindow : Page
 	{
-		private RecognitionPath.RecognitionPath recognitionPath;
-        Database.DatabaseService db;
-        MDetector md;
-        MarkerPosition3D pos;
-        ToolArtNetwork toolNetwork;
-        DisplayComponent displayContent;
-        PiktoViewManager piktoViewMan;
+        PiktoRecognitionAndXNAViewer pictoRecognitionAndXnaView;
         Image<Bgr, Byte> img;
-
+        Database.DatabaseService db;
         public LearningPathWindow()
         {
             InitializeComponent();
-            md = new MDetector();
-            pos = new MarkerPosition3D(80.0f, 640.0f, 640, 480);
             img = new Image<Bgr, byte>(640, 480, new Bgr(255, 255, 0));
-
+         
             db = new Database.DatabaseService();
-      
-            PiktoViewDB piktodb = new PiktoViewDB(db);
-            toolNetwork = new ToolArtNetwork(piktodb.getImageIdDic());
-            piktoViewMan = new PiktoViewManager(piktodb);
-			Camera camera = new Camera();
-			camera.TimeElapsed += new EventHandler<CameraEventArgs>(displayImage);
+            pictoRecognitionAndXnaView = new PiktoRecognitionAndXNAViewer();
+            pictoRecognitionAndXnaView.initialize(db);
+            
+            Camera camera = new Camera();
+            camera.TimeElapsed += new EventHandler<CameraEventArgs>(displayImage);
+        }
+        private void displayImage(object s, CameraEventArgs e)
+        {
+            img = e.Image;
+         }
+
+        
+        private void xnaLoad(object sender, GraphicsDeviceEventArgs e)
+        {
+            
+            pictoRecognitionAndXnaView.createScene((GraphicsDeviceControl)sender, e.GraphicsDevice);
         }
 
-		private void displayImage(object s, CameraEventArgs e)
-		{
-			img = e.Image;
-		}
+        private void renderXna(object sender, GraphicsDeviceEventArgs e)
+        {
+            pictoRecognitionAndXnaView.render(img);
+        }
 
-		private void xnaLoad(object sender, GraphicsDeviceEventArgs e)
-		{
-			displayContent = piktoViewMan.createScene(xnaHost, e.GraphicsDevice);
-		}
 
-		private void renderXna(object sender, GraphicsDeviceEventArgs e)
-		{
-			//md.findMarkers(img.Convert<Gray, Byte>());
-			//if (md.isMarker())
-			//{
-			//    int id = toolNetwork.recognitionPictograms(md.markers[0].getSymbolImage());
-			//    if (id != -1)
-			//    {
-			//        pos.estimate(md.markers[0]);
-			//        piktoViewMan.viewSceneMarker(id, pos.getTransformatinMatrix(), img.ToBitmap());
-			//    }
-			//}
-			//else
-			//{
-				piktoViewMan.updateDisplayCameraLayer(img.ToBitmap());
-			//}
-			displayContent.displaySetContent();
-
-		}
 	}
 }
